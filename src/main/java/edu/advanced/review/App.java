1e4c2b752c76dfe1978d29f8e05f4e187d4b9762
Juan J. Garcia <j2gl.edu@gmail.com>
2019-03-27 19:04:39 -0600

    Initial commit

---
+++ src/main/java/edu/advanced/review/App.java
@@ -0,0 +1,13 @@
+package edu.advanced.review;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
